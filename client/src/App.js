1350379e9997ef181ec96c73a86995336a54593e
Srikant114 <srikantsahu114@gmail.com>
2023-06-02 20:42:16 +0530

    Initial built

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {DiReact} from "react-icons/di"
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-red-500 text-center font-extrabold">Hello From react</h1>
+      <DiReact size={100}/>
+    </div>
+  );
+}
+
+export default App;
